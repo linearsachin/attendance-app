9b4704dba3785386980dfa1ffde826846daeb6f1
SacHin Yadav <sachineg39@gmail.com>
2020-03-05 22:55:30 +0530

    s

--- vAddentance/settings.py
+++ vAddentance/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')c5*x49ac+hq)hwwx_+-)4-2i4b(kpz*0@ddhq%*cu4!6&h5)%'
+SECRET_KEY = ['ATTENDANCE_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
